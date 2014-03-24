4b7ac8acc4605cc43f71040ff2f8c4008d398fd1
Peter Kinmond <peterkinmond@gmail.com>
2014-03-23 20:53:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HundredBestDays::Application.config.secret_key_base = 'caad628ee4612efabb7441376680eafd2c04acccb7921e7b7c071b6b9d9df3892466f97afad7618dba2e1f2ef8c71c3b35d12e104f76ec29e69c8cf51fae3d98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HundredBestDays::Application.config.session_store :cookie_store, key: '_hundred-best-days_session'
