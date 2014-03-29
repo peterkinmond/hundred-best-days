1d268dbcee768c65b6903f6430ea9f4041f3ae4e
Peter Kinmond <peterkinmond@gmail.com>
2014-03-29 14:28:52 -0700

    Add HAML

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.4'
+gem 'haml-rails', '~> 0.5.3'
 
 gem 'mysql2', '~> 0.3.11'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HundredBestDays::Application.routes.draw do
+  root 'home#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
