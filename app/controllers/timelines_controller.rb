5c54b657a13a994fc745b9a1dcecf01d2a854ed1
Peter Kinmond <peterkinmond@gmail.com>
2014-05-23 07:22:05 -0700

    Add years to timeline as set intervals

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,5 +1,6 @@
 class TimelinesController < ApplicationController
   def show
     @events = Event.all.sort_by(&:date)
+    @years = Event.range_of_years(@events)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ActiveRecord::Base
   belongs_to :user
 
+  def self.range_of_years(events)
+    earliest_event_year = events.map{|e| e.date.year}.min
+    current_year = Time.now.year
+
+    (earliest_event_year..current_year).to_a
+  end
 end
