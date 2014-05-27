b3bf58037d06be39f94b52f98b9ae0ea388b7c5f
Peter Kinmond <peterkinmond@gmail.com>
2014-05-26 22:05:20 -0700

    Add event description to timeline

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501145226) do
+ActiveRecord::Schema.define(version: 20140527045153) do
 
   create_table "events", force: true do |t|
     t.integer  "user_id"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140501145226) do
     t.datetime "updated_at"
     t.date     "date"
     t.string   "title"
+    t.string   "location"
+    t.text     "description"
   end
 
   create_table "users", force: true do |t|
