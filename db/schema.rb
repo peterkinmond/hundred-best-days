49300ecfc185a8d4e42c55e96af51c69b0f2a697
Peter Kinmond <peterkinmond@gmail.com>
2014-05-31 14:53:56 -0700

    Add user avatar in navbar
    * This is also fetched from call to Facebook
    * Storing as image URL for now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140527045153) do
+ActiveRecord::Schema.define(version: 20140531212055) do
 
   create_table "events", force: true do |t|
     t.integer  "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140527045153) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
