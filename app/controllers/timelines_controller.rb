8c3064fabe0dc856939ea702d70fb1c24cae4032
Peter Kinmond <peterkinmond@gmail.com>
2014-05-31 10:49:16 -0700

    Remove unused code (timeline no longer has independent list of years)

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,6 +1,5 @@
 class TimelinesController < ApplicationController
   def show
     @events = Event.all.sort_by(&:date)
-    @years = Event.range_of_years(@events)
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,12 +2,4 @@ require 'spec_helper'
 
 describe Event do
   it { should belong_to(:user) }
-
-  describe "#range_of_years" do
-    it "returns array with years from earliest event until present" do
-      event = Event.create(date: Time.now - 2.years, title: "Test Event")
-      events = [event]
-      expect(Event.range_of_years(events)).to eq([2012, 2013, 2014])
-    end
-  end
 end
