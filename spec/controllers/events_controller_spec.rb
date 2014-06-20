ad1ccf74891222bdad57931e3bbc0479211bc7a6
Peter Kinmond <peterkinmond@gmail.com>
2014-06-20 08:42:41 -0700

    Update UI to allow new events

---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe EventsController do
+  describe 'GET show' do
+    it 'breaks' do
+      expect(1).to eq(1)
+    end
+  end
+end
--- spec/controllers/timelines_controller_spec.rb
+++ spec/controllers/timelines_controller_spec.rb
@@ -3,8 +3,18 @@ require 'spec_helper'
 describe TimelinesController do
   describe 'GET show' do
     it "works" do
+      user = User.create!(name: 'Test User')
+      allow(controller).to receive(:current_user).and_return(user)
       get 'show'
       expect(response).to be_success
+    end
+
+    it "sets events to current user's events" do
+      user = User.create!(name: 'Test User')
+      event1 = Event.create!(user: user, title: 'Fake event')
+      allow(controller).to receive(:current_user).and_return(user)
+      get 'show'
+      expect(assigns(:events)).to eq([event1])
     end
   end
 end
