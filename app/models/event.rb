012f1a874dbe39f5f772169060bf200bc0ea3fbc
Peter Kinmond <peterkinmond@gmail.com>
2014-07-14 07:59:59 -0700

    Make location and date mandatory

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :title
+  validates_presence_of :location
+  validates_presence_of :date
   validates_presence_of :user
 
   enum privacy_status: [ :only_me, :invite, :everyone ]
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 
 describe Event do
   it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:location) }
+  it { should validate_presence_of(:date) }
   it { should belong_to(:user) }
 end
